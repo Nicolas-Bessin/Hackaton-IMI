d73bb8a8e7f247117b8b6248fd7b389885b2e4d5
Vincent <vboulard07@gmail.com>
2023-09-05 16:29:56 +0200

    added context

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from src.utils.ask_question_to_pdf import ask_question_to_pdf, gpt3_completion
 
 app = Flask(__name__)
 
+context = " "
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
 @app.route("/prompt", methods=["POST"])
 def prompt():
+    global context 
     question = request.form["prompt"]
-    answer = gpt3_completion(question)
+    answer = gpt3_completion(context + "\n" + question)
+    context = context + "\n" + question + "\n" + answer
     return {"answer" : answer }, 200
 
 
